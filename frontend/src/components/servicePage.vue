aad78451cd5480355d3afbfaf451539e8c994f72
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2023-03-13 18:08:52 -0500

    Update servicePage.vue

--- frontend/src/components/servicePage.vue
+++ frontend/src/components/servicePage.vue
@@ -22,7 +22,24 @@ export default {
 
 <template>
   <h1 v-if="status">ONLY EDITOR SHOULD SEE THIS</h1>
-  <div>
-    ANYONE CAN SEE THIS
-  </div>
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <h1>Services</h1>
+        <form @submit.prevent="viewevents">
+          <div class="form-group">
+          <label for="events">Name: </label>
+          <input type="text" id="events" v-model="events">
+          </div>
+          <div class="form-group">
+          <label for="title">Title: </label>
+          <input type="text" id="title" v-model="title">
+          </div>
+          <div class="form-group">
+          <label for="description">Description: </label>
+          <input type="text" id="description" v-model="description">
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
 </template>
