7e5b84df3a13d3cbeaafdb7b71222fe8e870a768
David Lim <davidlim1021@gmail.com>
2023-04-19 21:58:24 -0500

    made logout component

---
+++ frontend/src/components/logout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- Your component content here -->
+        <button @click="logout">Logout</button>
+    </div>
+</template>
+  
+<script>
+export default {
+    methods: {
+        logout() {
+            // Perform any logout logic, e.g., API call to invalidate session, clear local storage, etc.
+            // Then, use Vue.js router to navigate to the login page
+            this.$router.push({ name: 'loginPage.vue' });
+        }
+    }
+}
+</script>
