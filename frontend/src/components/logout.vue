bd092a6c787cdfb1caa79d16a3cce6b69b149649
David Lim <davidlim1021@gmail.com>
2023-04-19 23:41:26 -0500

    Logout created. But, tabs in navbar not cleared

--- frontend/src/components/logout.vue
+++ frontend/src/components/logout.vue
@@ -1,18 +1,61 @@
 <template>
-    <div>
-        <!-- Your component content here -->
-        <button @click="logout">Logout</button>
+    <div class="container">
+        <div class="center">
+            <button @click="logout" class="logout-button">Click here to Logout</button>
+            <p v-if="success" class="text-green-700 w-full flex justify-center">Logout Success!</p>
+        </div>
     </div>
+    <!-- <div v-if="success" class="text-green-700 w-full flex justify-center">Logout Success!</div> -->
 </template>
   
 <script>
 export default {
+    data() {
+        return {
+            success: false
+        }
+    },
     methods: {
         logout() {
-            // Perform any logout logic, e.g., API call to invalidate session, clear local storage, etc.
-            // Then, use Vue.js router to navigate to the login page
-            this.$router.push({ name: 'loginPage.vue' });
+            this.success = true;
+            sessionStorage.clear(); // Your logout logic here
+            console.log("Logged out");
         }
     }
-}
+};
 </script>
+  
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    /* Set container height to fill the viewport */
+}
+
+.center {
+    text-align: center;
+}
+
+.logout-button {
+    background-color: #ff0000;
+    /* Button background color */
+    color: #ffffff;
+    /* Button text color */
+    padding: 12px 24px;
+    /* Button padding */
+    font-size: 18px;
+    /* Button font size */
+    border: none;
+    /* Remove button border */
+    cursor: pointer;
+    /* Add cursor pointer on hover */
+}
+
+.logout-button:hover {
+    opacity: 0.8;
+    /* Reduce opacity on hover */
+}
+</style>
+  
