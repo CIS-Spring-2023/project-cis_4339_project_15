0eb1b63393605aee7e053cbc18fa01146096a541
David Lim <davidlim1021@gmail.com>
2023-04-22 21:15:44 -0500

    not fetching chartdata

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ mongoose
   })
 
 // declare port number for the api
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 27017
 
 // setup and access request body
 app.use(express.json())
@@ -41,6 +41,36 @@ app.use('/org', require('./routes/org'))
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
+// Define a schema for your data
+const eventSchema = new mongoose.Schema({
+  eventName: String,
+  eventDate: String,
+  numberOfAttendees: Number
+});
+
+// Create a model based on the schema
+const Event = mongoose.model('Event', eventSchema);
+
+// Fetch data from MongoDB
+app.get('/api/chartData', async (req, res) => {
+  try {
+    // Fetch data from MongoDB using the Event model
+    const events = await Event.find();
+
+    // Process the data and format it as needed for your chart
+    const chartData = events.map(event => ({
+      label: event.eventName,
+      value: event.numberOfAttendees
+    }));
+
+    // Send the data as response
+    res.json(chartData);
+  } catch (err) {
+    console.error('Failed to fetch chart data:', err);
+    res.status(500).json({ error: 'Failed to fetch chart data' });
+  }
+});
 
 // error handler
 app.use(function (err, req, res, next) {
--- frontend/src/components/barChart.vue
+++ frontend/src/components/barChart.vue
@@ -1,5 +1,6 @@
 <script>
 import { Chart, registerables } from 'chart.js'
+import axios from 'axios' // import Axios library for making HTTP requests
 Chart.register(...registerables)
 
 export default {
@@ -12,47 +13,63 @@ export default {
     }
   },
   async mounted() {
-    const backgroundColor = this.chartData.map(() => this.getColor())
-    const borderColor = backgroundColor.map((e) =>
-      e.replace(/[\d\.]+\)$/g, '1)')
-    )
-    await new Chart(this.$refs.attendanceChart, {
-      type: 'bar',
-      data: {
-        labels: this.label,
-        datasets: [
-          {
-            borderWidth: 1,
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            data: this.chartData
-          }
-        ]
-      },
-      options: {
-        scales: {
-          y: {
-            ticks: {
-              stepSize: 1
-            }
-          },
-          x: {
-            gridLines: {
-              display: false
-            }
-          }
-        },
-        plugins: {
-          legend: {
-            display: false
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: true
-      }
-    })
+    await this.fetchChartData() // Call the fetchChartData method on component mount
+    this.renderChart() // Call the renderChart method to render the chart
   },
   methods: {
+    async fetchChartData() {
+      try {
+        // Make an HTTP GET request to fetch chart data from backend API
+        const response = await axios.get('http://127.0.0.1:27017/api/chartData') // Update URL to match your backend route
+        const chartData = response.data // Assuming the response is an array of chart data objects
+        this.label = chartData.map(event => event.label) // Update label prop with fetched data
+        this.chartData = chartData.map(event => event.value) // Update chartData prop with fetched data
+      } catch (error) {
+        console.error('Failed to fetch chart data:', error)
+        // Handle error as needed
+      }
+    },
+    renderChart() {
+      const backgroundColor = this.chartData.map(() => this.getColor())
+      const borderColor = backgroundColor.map((e) =>
+        e.replace(/[\d\.]+\)$/g, '1)')
+      )
+      new Chart(this.$refs.attendanceChart, {
+        type: 'bar',
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              borderWidth: 1,
+              backgroundColor: backgroundColor,
+              borderColor: borderColor,
+              data: this.chartData
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              ticks: {
+                stepSize: 1
+              }
+            },
+            x: {
+              gridLines: {
+                display: false
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              display: false
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: true
+        }
+      })
+    },
     getColor() {
       let channel = () => Math.random() * 255
       return `rgba(${channel()}, ${channel()}, ${channel()}, 0.2)`
@@ -60,6 +77,7 @@ export default {
   }
 }
 </script>
+
 <template>
   <div class="shadow-lg rounded-lg overflow-hidden">
     <canvas class="p-10" ref="attendanceChart"></canvas>
