b5223c4e46efe4487d5204d8f1dc5ed3c5d09dc2
Andy Nguyen <89714005+AnGuiy@users.noreply.github.com>
2023-04-25 23:06:45 -0500

    fixed assessing edit for viewer

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -46,7 +46,10 @@ export default {
       this.getClients()
     },
     editClient(clientID) {
-      this.$router.push({ name: 'updateclient', params: { id: clientID } })
+      const ea = sessionStorage.getItem('edit')
+      if (ea) {
+        this.$router.push({ name: 'updateclient', params: { id: clientID } })
+      }
     }
   }
 }
@@ -54,78 +57,52 @@ export default {
 <template>
   <main>
     <div>
-      <h1
-        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-      >
+      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
         Find Client
       </h1>
     </div>
     <div class="px-10 pt-20">
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-      >
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
         <h2 class="text-2xl font-bold">Search Client By</h2>
         <!-- Displays Client Name search field -->
         <div class="flex flex-col">
           <select
             class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            v-model="searchBy"
-          >
+            v-model="searchBy">
             <option value="Client Name">Client Name</option>
             <option value="Client Number">Client Number</option>
           </select>
         </div>
         <div class="flex flex-col" v-if="searchBy === 'Client Name'">
           <label class="block">
-            <input
-              type="text"
+            <input type="text"
               class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="firstName"
-              v-on:keyup.enter="handleSubmitForm"
-              placeholder="Enter first name"
-            />
+              v-model="firstName" v-on:keyup.enter="handleSubmitForm" placeholder="Enter first name" />
           </label>
         </div>
         <div class="flex flex-col" v-if="searchBy === 'Client Name'">
           <label class="block">
-            <input
-              type="text"
+            <input type="text"
               class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="lastName"
-              v-on:keyup.enter="handleSubmitForm"
-              placeholder="Enter last name"
-            />
+              v-model="lastName" v-on:keyup.enter="handleSubmitForm" placeholder="Enter last name" />
           </label>
         </div>
         <!-- Displays Client Number search field -->
         <div class="flex flex-col" v-if="searchBy === 'Client Number'">
           <input
             class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            type="text"
-            v-model="phoneNumber"
-            v-on:keyup.enter="handleSubmitForm"
-            placeholder="Enter Client Phone Number"
-          />
+            type="text" v-model="phoneNumber" v-on:keyup.enter="handleSubmitForm"
+            placeholder="Enter Client Phone Number" />
         </div>
       </div>
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-      >
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
         <div></div>
         <div></div>
         <div class="mt-5 grid-cols-2">
-          <button
-            class="mr-10 border border-red-700 bg-white text-red-700 rounded"
-            @click="clearSearch"
-            type="submit"
-          >
+          <button class="mr-10 border border-red-700 bg-white text-red-700 rounded" @click="clearSearch" type="submit">
             Clear Search
           </button>
-          <button
-            class="bg-red-700 text-white rounded"
-            @click="handleSubmitForm"
-            type="submit"
-          >
+          <button class="bg-red-700 text-white rounded" @click="handleSubmitForm" type="submit">
             Search Client
           </button>
         </div>
@@ -134,9 +111,7 @@ export default {
 
     <hr class="mt-10 mb-10" />
     <!-- Display Found Data -->
-    <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-    >
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <div class="ml-10">
         <h2 class="text-2xl font-bold">List of Clients</h2>
         <h3 class="italic">Click table row to edit/display an entry</h3>
@@ -151,11 +126,7 @@ export default {
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
-            <tr
-              @click="editClient(client._id)"
-              v-for="client in queryData"
-              :key="client._id"
-            >
+            <tr @click="editClient(client._id)" v-for="client in queryData" :key="client._id">
               <td class="p-2 text-left">
                 {{ client.firstName + ' ' + client.lastName }}
               </td>
